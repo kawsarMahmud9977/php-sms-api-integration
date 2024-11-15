4711fa86adff10350c5b4b7b1b60c4bf9413bda0
kawsarMahmud9977 <nrain798@gmail.com>
2024-11-15 12:38:54 +0600

    initial commit

---
+++ send_sms.php
@@ -0,0 +1,35 @@
+<?php
+
+$message = "";
+
+function sms($dest_addr, $sms_text) {
+    $api_url = getenv('SMS_API_URL');
+    $username = getenv('SMS_API_USERNAME');
+    $password = getenv('SMS_API_PASSWORD');
+    $from = "YOUR_SENDER_NAME";
+
+    $fields = array(
+        'Username' => $username,
+        'Password' => $password,
+        'From' => $from,
+        'To' => $dest_addr,
+        'Message' => $sms_text
+    );
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $api_url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($fields));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+}
+
+$recipients = "8801783504771";
+$message = "Dear Sir,\r\nPlease Approve Pending Sales Order.\r\nDo No : 505\r\n";
+
+$sms_result = sms($recipients, $message);
+?>
---
+++ sms.php
@@ -0,0 +1,79 @@
+<?php 
+
+$massage="";
+
+    //sms
+ function sms($dest_addr,$sms_text){
+
+          
+
+       
+
+$url = "https://api.mobireach.com.bd/SendTextMessage?Username=aksid&Password=Akhr@2019&From=AKSID_HR";
+
+
+
+$fields = array(
+
+    'Username'      => "aksid",
+
+    'Password'      => "Akhrd@2023",
+
+    'From'          => "AKSID SALES",
+
+    'To'            => $dest_addr,
+
+    'Message'       => $sms_text
+
+);
+
+
+
+//open connection
+
+$ch = curl_init();
+
+
+
+//set the url, number of POST vars, POST data
+
+curl_setopt($ch, CURLOPT_URL, $url);
+
+curl_setopt($ch, CURLOPT_POST, count($fields));
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($fields));
+
+
+
+//execute post
+
+$result = curl_exec($ch);
+
+
+
+//close connection
+
+curl_close($ch);
+
+
+
+
+
+}
+
+//$str .= "Dear sir \r\n";
+//$str .= "Dear kawsar \r\n";
+
+
+
+$recipients=8801783504771;
+
+$massage="Dear kawsar,\r\n";
+
+$massage  = "Dear Sir,\r\nPlease Approve Pending Sales Order.\r\n";
+$massage.=  "Do No : 505\r\n";
+
+
+$sms_result=sms($recipients,$massage);
+
+?>
